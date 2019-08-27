4e70f01dce7904c5282caaeddaca3b085686a3ba
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2019-08-27 18:18:20 +0100

    Add background image to home page

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -5,23 +5,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.black87,
-          image: DecorationImage(
-            image: AssetImage("web/assets/images/home_background.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(            
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Header(),
-            Expanded(
-              child: Menu(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/home_background.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            color: Colors.black12,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Header(),
+              Expanded(
+                child: Menu(),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
